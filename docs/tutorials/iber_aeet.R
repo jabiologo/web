fc200bc2dc522463bbda539e10682f8b6d5e3362
jabiologo <javfer05@ucm.es>
2025-05-31 17:54:10 +0200

    update

---
+++ docs/tutorials/iber_aeet.R
@@ -0,0 +1,12 @@
+# Introduction to Bayesian Inference in Ecology (R & Nimble)
+# III Meeting SIBECOL / XVII Conference  AEET. June 2025
+
+#####################
+### 1. GLM BASICS ###
+#####################
+
+# Roe deer dropping count data
+roe <- read.csv("https://raw.githubusercontent.com/jabiologo/web/master/tutorials/roe_deer.csv")
+str(roe)
+head(roe)
+
