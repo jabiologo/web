0ae298601e972ee14e24ba1ad47a47d85a12e19f
jabiologo <javfer05@ucm.es>
2025-11-03 11:54:18 +0100

    fungi

---
+++ tutorials/upv_practicas2.R
@@ -0,0 +1,20 @@
+#########################################################################
+# Introducción a los Modelos Lineales: fundamentos teóricos y prácticos #
+# Universidad del País Vasco, octubre-noviembre 2025                    #
+#########################################################################
+
+###########################################
+# MÓDULO 2: EL MODELO LINEAL GENERALIZADO #
+###########################################
+
+# 2.1.1) Factores categóricos
+#  
+help(Distributions)
+
+# 2.1.2) Caso práctico, fungidicdas triazoles
+#  
+help(Distributions)
+
+# 2.2) Funciones vínculo: más allá de la distribución normal.
+# Conejos en carreteras
+help(Distributions)
