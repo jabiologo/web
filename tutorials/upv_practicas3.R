7fbc7c449b4e61526812d92644071d4b19111ea8
jabiologo <javfer05@ucm.es>
2025-11-24 14:09:22 +0100

    datos

---
+++ tutorials/upv_practicas3.R
@@ -0,0 +1,13 @@
+#########################################################################
+# Introducción a los Modelos Lineales: fundamentos teóricos y prácticos #
+# Universidad del País Vasco, octubre-noviembre 2025                    #
+#########################################################################
+
+################################################
+# MÓDULO 3: MODELOS MIXTOS Y OTRAS EXTENSIONES #
+################################################
+
+# 3.1) Modelos mixtos (factores aleatorios)
+
+garrapatas <- read.csv("https://raw.githubusercontent.com/jabiologo/web/master/tutorials/garrapatas_cotos.csv")
+
